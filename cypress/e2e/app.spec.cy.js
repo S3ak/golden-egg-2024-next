c604cc18dadeef5c3f282edf42ff58d89e906f9a
Monde Sineke <nsineke@gmail.com>
2024-03-18 17:26:11 +0100

    test: :white_check_mark: Create first test
    render the app with title

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -1,6 +1,6 @@
 describe("App", () => {
   it("Renders home page", () => {
     cy.visit("/");
-    cy.get("h1").should("contain", "Golden Egg Awards 2024");
+    cy.get("h1").should("contain", "Welcome to Golden Egg 2024");
   });
 });
