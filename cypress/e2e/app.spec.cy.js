d0cbcca56748881bcc0bc2e8f3536c22e5fd9125
Monde Sineke <nsineke@gmail.com>
2024-03-19 08:20:13 +0100

    docs: :memo: Update Readme

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -1,3 +1,8 @@
+const ENV_URL = Cypress.env("BASE_API_URL") || "http://localhost:3000/api";
+
+/**
+ * Most basic test to check the app runs.
+ */
 describe("App", () => {
   it("Renders home page", () => {
     cy.visit("/");
