842c6699d53b8be3bb8882582590decaf2822409
Monde Sineke <nsineke@gmail.com>
2024-05-15 10:34:21 +0200

    fix: stories

--- app/components/cta-card/index.stories.tsx
+++ app/components/cta-card/index.stories.tsx
@@ -4,7 +4,13 @@ import CTACard from "./index";
 const meta: Meta<typeof CTACard> = {
   component: CTACard,
   tags: ["autodocs"],
-  args: {},
+  args: {
+    href: "/",
+    target: "__blank",
+    imgSrc:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    children: "CTA Card",
+  },
   render: (args) => <CTACard {...args} />,
 };
 
