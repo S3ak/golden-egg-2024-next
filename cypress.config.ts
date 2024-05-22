4547faa4bb02fcb341bf240d2bd9e55d28cf75ee
Monde Sineke <nsineke@gmail.com>
2024-05-22 16:43:41 +0200

    chore: check env in e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
     },
   },
   env: {
-    BILLETTO_ACCESS_KEY_SECRET:
-      "3mHA1K,jItX4895EsDjUL4XygncH7NE8w9vBhJUXYtBWhFUa1H",
-    BILLETTO_APP_ID: "BLT2KPE281WX8RJTUWC3Z1BMG",
+    BILLETTO_ACCESS_KEY_SECRET: "",
+    BILLETTO_APP_ID: "",
   },
 });
